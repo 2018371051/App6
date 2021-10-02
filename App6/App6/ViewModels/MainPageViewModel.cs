cf2406852f8cb10124a51c1f17414708d5ef192d
Consuelo Barrios Rodríguez <2018371051@uteq.edu.mx>
2021-10-02 02:03:29 -0500

    Agregar archivos de proyecto.

---
+++ App6/App6/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,68 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        private string _userName;
+        private string _password;
+        private Command _loginCommand;
+        private bool isErrorLogin1;
+
+        public MainPageViewModel(INavigation navigation) : base(navigation)
+        {
+
+        }
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command LoginCommand
+        {
+            get => _loginCommand ?? (_loginCommand = new Command(GoToLoginAction));
+        }
+
+        public bool IsErrorLogin
+        {
+            get => isErrorLogin1; 
+            set
+            {
+                isErrorLogin1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void GoToLoginAction()
+        {
+            IsErrorLogin = !(string.Equals(UserName, "Consuelo") && string.Equals(Password, "123"));
+            if (!IsErrorLogin)
+            {
+                UserName = String.Empty;
+                Password = String.Empty;
+                Navigation.PushAsync(new PaginaInicio());
+
+            }
+        }
+    }
+
+}
---
+++ App6/App6/ViewModels/PaginaInicioViewModel.cs
@@ -0,0 +1,27 @@
+﻿using App6.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App6.ViewModels
+{
+    internal class PaginaInicioViewModel : BaseViewModel
+    {
+        Command _goToDetails;
+        Command _logOut;
+
+       
+
+        public PaginaInicioViewModel(INavigation navigation = null) : base(navigation)
+        {
+        }
+        public Command GoToDetailsCommand { get => _goToDetails ?? (_goToDetails = new Command(GoToDetailsAction)); }
+        public Command LogOutCommand { get => _logOut ?? (_logOut = new Command(() => Navigation.PopToRootAsync())); }
+
+        private void GoToDetailsAction()
+        {
+            Navigation.PushAsync(new DetailsPage());
+        }
+    }
+}
